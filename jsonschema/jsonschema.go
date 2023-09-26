4d6c83e7ca0584dd9debc0bf732722ecca020a2f
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-09-26 14:32:48 +0300

    Update jsonschema/jsonschema.go
    Co-authored-by: Herman Schaaf <hermanschaaf@gmail.com>

--- jsonschema/jsonschema.go
+++ jsonschema/jsonschema.go
@@ -7,7 +7,8 @@ import (
 	"github.com/invopop/jsonschema"
 )
 
-// Generate returns a formatted JSON schema for the parameter.
+// Generate returns a formatted JSON schema for the input struct, according to the tags
+// defined by https://github.com/invopop/jsonschema
 func Generate(a any) ([]byte, error) {
 	sc := (&jsonschema.Reflector{RequiredFromJSONSchemaTags: true}).ReflectFromType(reflect.TypeOf(a))
 	return json.MarshalIndent(sc, "", "  ")
