25fe25c6b66275bf21d721547e385cc998f21867
candiduslynx <candiduslynx@gmail.com>
2023-09-26 13:22:37 +0300

    add example doc

--- jsonschema/jsonschema.go
+++ jsonschema/jsonschema.go
@@ -7,6 +7,7 @@ import (
 	"github.com/invopop/jsonschema"
 )
 
+// Generate returns a formatted JSON schema for the parameter.
 func Generate(a any) ([]byte, error) {
 	sc := (&jsonschema.Reflector{RequiredFromJSONSchemaTags: true}).ReflectFromType(reflect.TypeOf(a))
 	return json.MarshalIndent(sc, "", "  ")
