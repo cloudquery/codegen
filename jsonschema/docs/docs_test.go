a7d38b786f3bd04f1d2c32caef9d57a48ea8ca5e
bbernays <ben@bernays.me>
2025-03-31 13:27:23 -0500

    Update docs_test.go

--- jsonschema/docs/docs_test.go
+++ jsonschema/docs/docs_test.go
@@ -2,10 +2,12 @@ package docs
 
 import (
 	"embed"
+	"encoding/json"
 	"strings"
 	"testing"
 
 	"github.com/bradleyjkemp/cupaloy/v2"
+	"github.com/invopop/jsonschema"
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,22 +31,45 @@ func genSnapshot(t *testing.T, fileName string) {
 	cupaloy.New(cupaloy.SnapshotFileExtension(".md")).SnapshotT(t, normalizeContent(doc))
 }
 
+func genSnapshotStruct(t *testing.T, fileName string) {
+	data, err := schemaFS.ReadFile(fileName)
+	require.NoError(t, err)
+
+	var root jsonschema.Schema
+	err = json.Unmarshal(data, &root)
+
+	require.NoError(t, err)
+
+	doc, err := GenerateFromSchema(root, 1)
+	require.NoError(t, err)
+
+	cupaloy.New(cupaloy.SnapshotFileExtension(".md")).SnapshotT(t, normalizeContent(doc))
+}
+
 func TestAWS(t *testing.T) {
 	genSnapshot(t, "testdata/aws.json")
+	genSnapshotStruct(t, "testdata/aws.json")
 }
 
 func TestGCP(t *testing.T) {
 	genSnapshot(t, "testdata/gcp.json")
+	genSnapshotStruct(t, "testdata/gcp.json")
 }
 
 func TestClickHouse(t *testing.T) {
 	genSnapshot(t, "testdata/clickhouse.json")
+	genSnapshot(t, "testdata/clickhouse.json")
+
 }
 
 func TestFiletypes(t *testing.T) {
 	genSnapshot(t, "testdata/filetypes.json")
+	genSnapshot(t, "testdata/filetypes.json")
+
 }
 
 func TestFileDestination(t *testing.T) {
 	genSnapshot(t, "testdata/file-destination.json")
+	genSnapshot(t, "testdata/file-destination.json")
+
 }
